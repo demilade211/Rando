fe2cff9641c101d558e1c6b699d773468228ea35
demilade211 <50405225+demilade211@users.noreply.github.com>
2019-08-09 12:01:13 +0100

    Add files via upload

--- home.js
+++ home.js
@@ -1,10 +1,24 @@
 const burger = document.querySelector(".burger");
 const nav = document.querySelector(".mobile-nav");
 const navLink = document.querySelectorAll(".mobile-nav li");
-burger.addEventListener('click',toggleNav)
 // fuction to toggle nav
-function toggleNav(){
-    nav.classList.toggle('nav-active');
+let state = 0;
+const openDrop=()=> {
+  nav.style.display = "flex";
+}
+const closeDrop=()=>{
+nav.style.display = "none";
+state = 0;
+}
+
+function toggleNav(n){
+  state += n;
+   if(state==1){
+     openDrop();
+   }
+   else{
+     closeDrop();
+   }
     // to animate nav
 navLink.forEach((link,index) => {
   if(link.style.animation){ // without the if statement the list fade in will only implent on refresh of the page
