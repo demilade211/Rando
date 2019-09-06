522d8a66d55a95fdbd1393d164b506fe7a73a1e5
demilade211 <50405225+demilade211@users.noreply.github.com>
2019-09-06 10:42:49 +0100

    Add files via upload

--- home.js
+++ home.js
@@ -1,6 +1,7 @@
 const burger = document.querySelector(".burger");
 const nav = document.querySelector(".mobile-nav");
 const navLink = document.querySelectorAll(".mobile-nav li");
+const mobile = document.querySelectorAll(".m-link");
 // fuction to toggle nav
 let state = 0;
 const openDrop=()=> {
@@ -19,6 +20,7 @@ function toggleNav(n){
    else{
      closeDrop();
    }
+
     // to animate nav
 navLink.forEach((link,index) => {
   if(link.style.animation){ // without the if statement the list fade in will only implent on refresh of the page
@@ -31,3 +33,8 @@ navLink.forEach((link,index) => {
 // to animate the burger icon
     burger.classList.toggle("toggle");
 }
+// to close mobile nav bar on click
+const closeDropdownMenuSelectingItem = (() => mobile.forEach((item) => item.addEventListener("click", ()=>{
+closeDrop();
+toggleNav();
+})))()
